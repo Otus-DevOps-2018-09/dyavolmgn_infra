187f3eb6c846112fdcd929bbd7b3dba9e9f5f0bc
root <root@example.com>
2018-10-21 13:00:40 +0300

    fix deploy mongodb sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 echo "Starting mongodb installation ..."
 echo "Applying key for mongodb repo ..."
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 echo "Trying to add mongodb repo to sources list..."
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 echo "Checking for package index update ..."
-sudo apt-get update
+apt-get update
 echo "Going to install mongodb"
-sudo apt-get install -y mongodb-org
-echo "Starting database service ..."
-sudo systemctl start mongod
-echo -e "Installed!\nSystemd service startup turning on ..."
-sudo systemctl enable mongod
+apt-get install -y mongodb-org
+echo "Starting MONGODB ..."
+systemctl start mongod
+echo -e "enable MONGODB ..."
+systemctl enable mongod
