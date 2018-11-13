f7e8e138f0feb935f63cef3793ddddc5092084a7
Konstantin Babushkin <devil.mgn@gmail.com>
2018-11-13 13:21:53 +0300

    Задание со *

--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "apps_external_ips" {
-  value = "${module.app.app_external_ip}"
-}
-
-#output "balancer_external_ip" {
-#  value = "${google_compute_forwarding_rule.reddit-app-balancer.ip_address}"
-#}
-
